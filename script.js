b3c8f1fbcbf27c38f70dbbcbbcc78364e02f7434
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-04-08 11:13:26 +0500

    g

---
+++ script.js
@@ -0,0 +1,24 @@
+/// Array
+
+let studentData = ["Saqib", "Shahid", "Hassan", "Ali", "Azan", "Rahman", "Bilal"];
+ studentData.shift(); // removes the first element from an array and returns it
+console.log(studentData);
+
+let studentData1 = ["Saqib", "Shahid", "Hassan", "Ali", "Azan", "Rahman", "Bilal"];
+
+studentData1.unshift(); // adds one or more elements to the beginning of an array
+console.log(studentData1);
+
+
+
+let studentData2 = ["Saqib", "Shahid", "Hassan", "Ali", "Azan", "Rahman", "Bilal"];
+
+studentData2.push("Shakir"); // adds one or more elements to the end of an array
+console.log(studentData2);
+
+
+let studentData3 = ["Saqib", "Shahid", "Hassan", "Ali", "Azan", "Rahman", "Bilal"];
+
+
+
+console.log
